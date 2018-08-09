f14a2e1cc29494ad655de0d41adec40ddcf1be57
haru-11 <degmyb.5534g@gmail.com>
2018-08-09 23:34:52 +0900

    add class

--- MCP3002_test.py
+++ MCP3002_test.py
@@ -3,11 +3,25 @@ import time
 
 pi = pigpio.pi()
 
-h = pi.spi_open(0, 75000, 0)
+class MCP3002:
 
-while True:
+	def __init__(self):
+		pi = pigpio.pi()
+		self.h = pi.spi_open(0, 75000, 0)
 
-	c, d =pi.spi_xfer(h,[0x68,0x00])
+	def get_ADC(self):
+		c, d =pi.spi_xfer(self.h,[0x68,0x00])
+		return (d[0]<<8)+d[1]
 
-	print str((d[0]<<8)+d[1])
-	time.sleep(1)
+
+adc =  MCP3002()
+
+try:
+
+	while True:
+		d = adc.get_ADC()
+		time.sleep(1.0)
+		print(str(d))
+
+except KeyboardInterrupt:
+	pass
